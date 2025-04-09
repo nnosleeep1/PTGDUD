549f0305df8704a94c5bdfc78baa4379604f9a7b
NhanNoNoob <nhantran.03042021@gmail.com>
2025-04-09 22:35:02 +0700

    2.API OverView

--- Lab05/User_App/src/pages/Home.jsx
+++ Lab05/User_App/src/pages/Home.jsx
@@ -5,13 +5,34 @@ import Menu from "../component/Menu"
 
 const Home=()=>{
     const [data, setData] = useState([]);
-
+     const fetchData= async  ()=>{
+        try {
+            const response = await fetch('http://localhost:3000/users');
+            const result = await response.json();
+            const formatted = result.map((item, index) => ({
+              ...item,
+              key: item.id || item.key || index.toString(),
+            }));
+            setData(formatted);
+          } catch (err) {
+            console.error('Error fetching data:', err);
+          }
+        };
+     useEffect(() => {
+        fetchData();
+       
+      }, []);
+      useEffect(() => {
+        console.log("data "+ JSON.stringify(data, null, 2))
+       
+      }, [data]);
+     
     return(
        <div className="flex pl-10 pr-10">
            <div><Menu/></div>
             <div className="w-full px-5">
                 <Header/>
-                 <Body />
+                 <Body data={data} setData={setData} fetchData={fetchData}/>
             </div>
             <div></div>
        </div>
