be2a0bdf130114c02545eebf6abc22218ed61ebb
NhanNoNoob <nhantran.03042021@gmail.com>
2025-04-09 22:42:08 +0700

    3+4.DataTable ReactJS and DataTable ReactJS API

--- Lab05/User_App/src/component/Body.jsx
+++ Lab05/User_App/src/component/Body.jsx
@@ -1,14 +1,14 @@
 
-import { useState } from "react"
+import Table from "./Table"
 import OverView from "./OverView";
 
-const Body=()=>{
+const Body=({data,setData,fetchData})=>{
     
     return(
         <div className="w-full p-5 border-2 border-[#EDEDED]">
            
             <OverView    />
-           
+            <Table data={data} setData={setData} fetchData={fetchData} />
           
         </div>
     )
